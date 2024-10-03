187764eb4837d300a4b4d759dfbbfa6ea726fff1
a-fashola01 <128617918+a-fashola01@users.noreply.github.com>
2024-10-03 12:42:38 -0400

    Constants updates

--- HighLow_Number_Guessing/Program.cs
+++ HighLow_Number_Guessing/Program.cs
@@ -5,36 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Random rnd = new Random();
-        const int MAX_GUESS = 10;
+        const int MAX_GUESSES = 10;
         const int FIRST = 1;
+        const int CLOSE_PROXIMITY = 5;
+        const int LOW_GUESS_RANGE = 1;
+        const int HIGH_GUESS_RANGE = 101;
         
         
 
         int numberOfGuesses = 1;
         int numberGuess = 0;
-        int correctGuess = rnd.Next(1, 100); 
+        int correctGuess = rnd.Next(LOW_GUESS_RANGE, HIGH_GUESS_RANGE); 
         
         //Console.WriteLine(correctGuess);
         
-        Console.WriteLine($"Hello, let's play a game! You will be guessing a number between 1 and 100, you will have {MAX_GUESS} tries");
+        Console.WriteLine($"Hello, let's play a game! You will be guessing a number between 1 and 100, you will have {MAX_GUESSES} tries");
         
-        while (numberOfGuesses <= MAX_GUESS || numberGuess != correctGuess )
+        while (numberOfGuesses <= MAX_GUESSES )
         {
             if (numberOfGuesses == FIRST)
             {
                 Console.WriteLine("Please enter your first guess: ");
             }
-            if (numberOfGuesses > FIRST && numberOfGuesses < MAX_GUESS)
+            if (numberOfGuesses > FIRST && numberOfGuesses < MAX_GUESSES)
             {
                 Console.WriteLine($"Please enter guess #{numberOfGuesses} : ");
             }
-            if (numberOfGuesses == MAX_GUESS)
+            if (numberOfGuesses == MAX_GUESSES)
             {
                 Console.WriteLine("Please enter your final guess: ");
             }
             numberGuess = Convert.ToInt32(Console.ReadLine());
             
-            if (numberGuess != correctGuess && numberOfGuesses >= MAX_GUESS)
+            if (numberGuess != correctGuess && numberOfGuesses >= MAX_GUESSES)
             {
                 Console.WriteLine("You lose!");
                 break;
@@ -44,7 +47,7 @@ class Program
                 Console.WriteLine("You Won " + correctGuess + " is the right answer!");
                 break;
             }
-            if (Math.Abs(numberGuess-correctGuess) <=5)
+            if (Math.Abs(numberGuess-correctGuess) <= CLOSE_PROXIMITY)
             {
                 Console.WriteLine("You're close!");
             }
